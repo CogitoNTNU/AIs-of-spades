3fd31f13061cf0c95b3efa2936b04d1c9a37cc0c
Daniel KM <dkm@meling.com>
2026-03-05 19:54:33 +0100

    draft 2 learningloop

--- src/learning_loop.py
+++ src/learning_loop.py
@@ -18,61 +18,19 @@ class LearningLoop():
             batch_trajectories = []
             batch_rewards = []
             for x in range(BATCH_SIZE):
-                reset_env()
-                episode=EpisodeLoop(HANDS_PER_EPISODE)
-                batch_trajectories.append(episode.get_trajectories())
-                batch_rewards.append(episode.get_rewards())
+                episode=TrainingEpisode(HANDS_PER_EPISODE)
+                reward = episode.play()
+                batch_rewards.append(reward)
+                batch_trajectories.append(episode.trajectory)
 
-            reward=compute_reward(batch_trajectories,batch_rewards)
-            result=gradient_decent(reward)
+            reward=self.compute_reward(batch_trajectories, batch_rewards)
+            result=self.gradient_decent(reward)
             if epoch % 1000 == 0: 
-                save_to_file(result)
+                self.save_to_file(result,epoch)
 
 
     def compute_reward():
         pass
 
-    def save_to_file():
-        pass
-
-if __name__ == "__main__":
-    learning_loop = LearningLoop()
-
-
-
-
-
-
-
-
-
-
-
-
-iteration = 1
-while True:
-    if iteration % 50 == 0:
-        table.hand_history_enabled = True
-    active_players = np.random.randint(2, 7)
-    table.n_players = active_players
-    obs = table.reset()
-    for agent in agents:
-        agent.reset()
-    acting_player = int(obs[indices.ACTING_PLAYER])
-    while True:
-        action = agents[acting_player].get_action(obs)
-        obs, reward, done, _ = table.step(action)
-        if  done:
-            # Distribute final rewards
-            for i in range(active_players):
-                agents[i].rewards.append(reward[i])
-            break
-        else:
-            # This step can be skipped unless invalid action penalty is enabled, 
-            # since we only get a reward when the pot is distributed, and the done flag is set
-            agents[acting_player].rewards.append(reward[acting_player])
-            acting_player = int(obs[indices.ACTING_PLAYER])
-    iteration += 1
-    table.hand_history_enabled = False
-
-
+    def save_to_file(result,epoch):
+        torch.save(result, f"SUPER_AWESOME_POKER_NEURAL_NETWORK_EPOCH_NUMBER:{epoch}")
