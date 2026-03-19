b9a4c064fb610b8715bf14a1e8e35d48b6258993
Simooo45 <simone.dav.elia@gmail.com>
2026-03-19 22:14:09 +0100

    Bugfixing

--- src/pokerenv/observation.py
+++ src/pokerenv/observation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from pokerenv.common import PlayerAction
 
 
 class CardObservation:
@@ -11,14 +12,14 @@ class CardObservation:
 class HandObservation:
     def __init__(self, cards):
         self.cards = [
-            CardObservation(cards[i : i + 2]) for i in range(0, len(cards) - 1, 2)
+            CardObservation(cards[i : i + 2]) for i in range(0, len(cards), 2)
         ]
 
 
 class TableObservation:
     def __init__(self, cards):
         self.cards = [
-            CardObservation(cards[i : i + 2]) for i in range(0, len(cards) - 1, 2)
+            CardObservation(cards[i : i + 2]) for i in range(0, len(cards), 2)
         ]
 
 
@@ -36,17 +37,14 @@ class ActionsObservation:
     def __init__(self, actions):
         self.actions = actions
 
-    def can_check(self):
-        return self.actions[0] == 1
-
     def can_fold(self):
-        return self.actions[1] == 1
+        return self.actions[PlayerAction.FOLD] == 1
 
     def can_bet(self):
-        return self.actions[2] == 1
+        return self.actions[PlayerAction.BET] == 1
 
     def can_call(self):
-        return self.actions[3] == 1
+        return self.actions[PlayerAction.CALL] == 1
 
 
 class BetRangeObservation:
@@ -58,22 +56,22 @@ class BetRangeObservation:
 class Observation:
     def __init__(self, obs_matrix, hand_log):
         self.player_identifier = obs_matrix[0]
-        self.actions = ActionsObservation(obs_matrix[1:5])
-        self.bet_range = BetRangeObservation(obs_matrix[5:7])
-        self.player_position = obs_matrix[7]
-        self.hand_cards = HandObservation(obs_matrix[8:12])
-        self.player_stack = obs_matrix[12]
-        self.player_money_in_pot = obs_matrix[13]
-        self.bet_this_street = obs_matrix[14]
-        self.street = obs_matrix[15]
-        self.table_cards = TableObservation(obs_matrix[16:26])
-        self.pot = obs_matrix[26]
-        self.bet_to_match = obs_matrix[27]
-        self.minimum_raise = obs_matrix[28]
+        self.actions = ActionsObservation(obs_matrix[1:4])
+        self.bet_range = BetRangeObservation(obs_matrix[4:6])
+        self.player_position = obs_matrix[6]
+        self.hand_cards = HandObservation(obs_matrix[7:11])
+        self.player_stack = obs_matrix[11]
+        self.player_money_in_pot = obs_matrix[12]
+        self.bet_this_street = obs_matrix[13]
+        self.street = obs_matrix[14]
+        self.table_cards = TableObservation(obs_matrix[15:25])
+        self.pot = obs_matrix[25]
+        self.bet_to_match = obs_matrix[26]
+        self.minimum_raise = obs_matrix[27]
 
         self.others = [
-            OtherPlayerObservation(obs_matrix[29 + i * 6 : 29 + i * 6 + 6])
-            for i in range((len(obs_matrix) - 29) // 6)
+            OtherPlayerObservation(obs_matrix[28 + i * 6 : 28 + i * 6 + 6])
+            for i in range((len(obs_matrix) - 28) // 6)
         ]
         self.hand_log = hand_log
         self.is_replay = False
@@ -84,7 +82,7 @@ class Observation:
 
     @staticmethod
     def empty():
-        obs_array = np.zeros(59, dtype=np.float32)
+        obs_array = np.zeros(58, dtype=np.float32)
         hand_log = np.full((32, 4), -1.0, dtype=np.float32)
         return Observation(obs_array, hand_log)
 
@@ -92,7 +90,6 @@ class Observation:
         hand = [(c.suit, c.rank) for c in self.hand_cards.cards]
         table = [(c.suit, c.rank) for c in self.table_cards.cards]
         actions = {
-            "check": self.actions.can_check(),
             "fold": self.actions.can_fold(),
             "bet": self.actions.can_bet(),
             "call": self.actions.can_call(),
